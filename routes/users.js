670d88197aea8c77f8b861c0a0690af3a89b0d5a
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-01-20 19:43:06 -0800

    adding validationResult on user route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 const user = require('../models/User');
 // @route  POST api/users
@@ -7,8 +8,24 @@ const user = require('../models/User');
 //@access  Public
 
  //pretains to api/users
-router.post('/', (req, res) => {
-  res.send(req.body)
+router.post('/', [
+  //checks name, email and password that can be automatically done using require on line 3.
+
+  check('name', 'Pleae add name')
+  .not()
+  .isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check(
+    'password', 'Please enter a password with 6 or more characters'
+  ).isLength({ min: 6 })
+], 
+(req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  res.send("Passed!")
 });
 
 module.exports = router;
