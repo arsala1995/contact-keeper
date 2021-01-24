8b05c1c988b8fef1772ab080329b26d6b9811436
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-01-24 11:16:45 -0800

    adding routes in contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -26,8 +26,35 @@ router.get('/', auth, async (req, res) => {
 //@access  Private
 
  //pretains to api/contacts
- router.post('/', (req, res) => {
-  res.send("Add contacts")
+ router.post('/', [auth, [
+   check('name', 'Name is required').not().isEmpty()
+ ]], 
+ async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    //after requesting body if error occurs
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, phone, type } = req.body;
+
+  try {
+    const newContact = new Contact({
+      name,
+      email,
+      phone, 
+      type,
+      user: req.user.id
+
+    });
+
+    const contact = await newContact.save();
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+
 });
 
 // @route  PUT api/contacts
