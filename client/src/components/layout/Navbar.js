be4d15a8f7e3419e6eaf63961e745d6a3349ebef
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-01-27 19:11:11 -0800

    adding Navbar to the app

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <i lassName={icon}> {title} </i>
+      </h1>
+      
+    </div>
+  )
+}
+
+Navbar.prototypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+}
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt'
+}
+export default Navbar;
