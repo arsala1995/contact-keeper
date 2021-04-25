bf2ee3fe6d4db63990e87c44f260046514a2e28c
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-04-24 17:43:09 -0700

    import types into contact state and also creatiing dummy contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,4 +1,41 @@
 import React, { useReducer } from 'react';
 import uuid from 'uuid';
-import ContactContext from './contactContext'
-import contactReducer from './contactReducer'
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT, 
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Sara Watson',
+        email: 'sara@gmail.com',
+        phone: '222-222-2222',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'Tommy',
+        email: 'tommy@gmail.com',
+        phone: '222-222-2222',
+        type: 'personal'
+      },
+      {
+        id: 3,
+        name: 'Arthur',
+        email: 'arthur@gmail.com',
+        phone: '222-222-2222',
+        type: 'professional'
+      }
+    ]
+  }
+}
