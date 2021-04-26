80c172f44fc40235171c2c03bac8bee49ce8050d
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-04-26 14:42:10 -0700

    adding contacts to home

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,15 +1,15 @@
-import React, { Fragmant, useContext} from 'react';
+import React, { Fragmant, Fragment, useContext} from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
   const { contacts } = contactContext;
-  
+
   return (
-    <div>
-      
-    </div>
+   <Fragment>
+     {contacts.map(contact => <h3>{contact.name}</h3>)}
+   </Fragment>
   )
 }
 
