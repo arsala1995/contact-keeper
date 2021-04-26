89e313e4bf32e8a3a7adfb04d8f71c9312a14cfc
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-04-26 14:25:47 -0700

    requiring contactcontext and creating contact component

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,12 @@
+import React, { Fragmant, useContext} from 'react';
+import ContactContext from '../../context/contact/contactContext';
+
+const Contacts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Contacts
