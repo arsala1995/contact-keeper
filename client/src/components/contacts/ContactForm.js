aa68bec0aa7f3185e3917f03e2773dcf6e7b2c70
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-05-10 16:24:54 -0700

    adding a new form in contactForm and putting inputs inside it

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -9,9 +9,10 @@ export const ContactForm = () => {
   });
   
   return (
-    <div>
-      
-    </div>
+    <form>
+      <h2 className="text-primary">Add Contact</h2>
+      <input type="text" placeholder="name" name="name" value={name} onChange={onChange} />
+    </form>
   )
 }
 export default ContactForm
