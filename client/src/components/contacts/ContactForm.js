53505e63294f5d3e82e0332e539f9b7bbd181f6e
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-05-10 16:26:22 -0700

    adding input field for email in form

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -11,7 +11,19 @@ export const ContactForm = () => {
   return (
     <form>
       <h2 className="text-primary">Add Contact</h2>
-      <input type="text" placeholder="name" name="name" value={name} onChange={onChange} />
+      <input 
+      type="text" 
+      placeholder="name" 
+      name="name" value={name} 
+      onChange={onChange} 
+      />
+      <input 
+      type="email" 
+      placeholder="Email" 
+      name="email"
+       value={email} 
+      onChange={onChange} 
+      />
     </form>
   )
 }
