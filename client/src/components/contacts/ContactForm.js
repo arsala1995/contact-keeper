14a914b24b123f5bc132bcab2e57ec2b714880c1
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-05-17 19:28:37 -0700

    adding contact contact reducer and adding onchanges to personal and professional in contactform

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -53,8 +53,10 @@ export const ContactForm = () => {
       />
       <h5>Contact Type</h5>
       <input type="radio" name="type" value="personal" checked={type === 'personal'}
+      onChange={onChange} 
       /> Personal {' '}
        <input type="radio" name="type" value="professional" checked={type === 'professional'}
+       onChange={onChange} 
       /> Professional {' '}
     <div>
       <input type="submit" value="Add Contact" className="btn btn-primary" />
