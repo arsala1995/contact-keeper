e06659f390b93ac04469fecb59680dd23d278fea
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-06-02 18:31:26 -0700

    adding update contact in reducer

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -15,6 +15,11 @@ export default (state, action) => {
         ...state,
         contacts: [...state.contacts, action.payload]
       };
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map(contact => contact.id === action.payload.id )
+      }
     case DELETE_CONTACT:
       return {
         ...state,
