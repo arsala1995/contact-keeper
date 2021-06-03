149a8ea8420b8578d6d6275da0719e27419aefea
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-06-02 18:32:14 -0700

    adding condition for the update contact state

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -18,7 +18,7 @@ export default (state, action) => {
     case UPDATE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.map(contact => contact.id === action.payload.id )
+        contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)
       }
     case DELETE_CONTACT:
       return {
